07bf0a0475f057bda919c33652db0be1e3ebec7f
buru <buruiana.stefan@gmail.com>
2026-05-25 13:01:59 +0200

    feat: db for login

--- server/app/routers/auth.py
+++ server/app/routers/auth.py
@@ -1,7 +1,10 @@
+from datetime import datetime, timezone
+
 from fastapi import APIRouter, HTTPException
 from app.schemas.auth import RegisterRequest, LoginRequest, SaltRequest
 from app.services import auth as auth_service
 from app.models.user import User
+from app.models.session import UserSession as Session
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
@@ -34,20 +37,34 @@ async def register(body: RegisterRequest, db: AsyncSession = Depends(get_db)):
 
 
 @router.post("/login", status_code=200)
-def login(body: LoginRequest):
-    if body.email not in fake_user_db:
+async def login(body: LoginRequest, db: AsyncSession = Depends(get_db)):
+
+    result = await db.execute(select(User).where(User.email == body.email))
+    user = result.scalar_one_or_none()
+
+    if not user:
         raise HTTPException(status_code=400, detail="Invalid email or password")
 
-    user = fake_user_db[body.email]
-    if not auth_service.verify(body.auth_key, user["auth_key_hash"]):
+    if not auth_service.verify(body.auth_key, user.auth_key_hash):
         raise HTTPException(status_code=400, detail="Invalid email or password")
 
-    full_access_token = auth_service.create_full_access_token(user_id=body.email)
-    limited_access_token = auth_service.create_limited_access_token(user_id=body.email)
+    full_access_token = auth_service.create_full_access_token(user_id=user.id.hex)
+    limited_access_token = auth_service.create_limited_access_token(user_id=user.id.hex)
 
-    fake_user_db[body.email]["sessions"][body.device] = {
-        "full_access_token_hash": auth_service.hash(full_access_token),
-    }
+    result = await db.execute(
+        select(Session).where(
+            Session.user_id == user.id
+            and Session.expires_at > datetime.now(timezone.utc)
+            and Session.device_name == body.device
+        )
+    )
+    session = result.scalar_one_or_none()
+
+    if session:
+        session.full_access_token_hash = auth_service.hash(full_access_token)
+        session.expires_at = datetime.now(timezone.utc) + auth_service.timedelta(
+            days=auth_service.settings.FULL_ACCESS_TOKEN_EXPIRE_DAYS
+        )
 
     return {
         "message": "User logged in successfully",
